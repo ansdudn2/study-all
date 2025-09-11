562c53ec5e4313c7dc77a557fee944466749bb5f
ansdudn2 <ansdudn2@naver.com>
2025-09-11 18:33:37 +0900

    feat: variable questions

---
+++ java-one/src/variable/ex/VarEx1Question.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx1Question {
+    public static void main(String[] args) {
+        //나오는 숫자 4,3을 다른 숫자로 한번에 변경할 수 있도록 num1,num2 를 사용하여 변경해보기
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+    }
+
+}
---
+++ java-one/src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVal = 10000000000L;
+        System.out.println(longVal);
+
+        boolean booleanVal = true;
+        System.out.println(booleanVal);
+    }
+}
