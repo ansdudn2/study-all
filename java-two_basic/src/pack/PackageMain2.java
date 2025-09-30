2f912ce54526406a5376512fa3be39dba31159c8
ansdudn2 <ansdudn2@naver.com>
2025-10-01 01:11:33 +0900

    feat:PackageMain2-> ref: add import in PackageMain1

---
+++ java-two_basic/src/pack/PackageMain2.java
@@ -0,0 +1,9 @@
+package pack;
+
+import pack.a.User;
+public class PackageMain2 {
+    public static void main(String[] args) {
+        Data d = new Data();
+        User user = new User();
+    }
+}
