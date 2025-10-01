578360cc9316a612721a8791131583cf679e9d5b
ansdudn2 <ansdudn2@naver.com>
2025-10-01 22:24:04 +0900

    ref:SpaekerMain comment out direct access

--- java-two_basic/src/access/SpeakerMain.java
+++ java-two_basic/src/access/SpeakerMain.java
@@ -15,8 +15,10 @@ public class SpeakerMain {
         speaker.showVolume();
 
         // 필드에 직접 접근
+        /* volume 을 private로 변경 후 컴파일 오류
         System.out.println("volume 필드 직접 접근 수정");
         speaker.volume = 200;
         speaker.showVolume();
+         */
     }
 }
