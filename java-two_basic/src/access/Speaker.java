d76fec111f84e8c0033cc53434c1bbc00e3ea4c9
ansdudn2 <ansdudn2@naver.com>
2025-10-01 22:13:48 +0900

    ref:speaker volumup if > -> >= & feat:SpeakerMain

--- java-two_basic/src/access/Speaker.java
+++ java-two_basic/src/access/Speaker.java
@@ -9,7 +9,7 @@ public class Speaker {
     }
 
     void volumeup() {
-        if (volume > 100) {
+        if (volume >= 100) {
             System.out.println("음량을 증가할 수 없습니다. 최대 음량입니다.");
         } else {
             volume += 10;
