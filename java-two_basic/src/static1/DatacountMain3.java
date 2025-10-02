e6193b9455235bdacda24c47209df7e8cf2ff90f
ansdudn2 <ansdudn2@naver.com>
2025-10-03 00:23:42 +0900

    feat: Data3

---
+++ java-two_basic/src/static1/DatacountMain3.java
@@ -0,0 +1,15 @@
+package static1;
+
+public class DatacountMain3 {
+    public static void main(String[] args) {
+        Data3 data1 = new Data3("A");
+        System.out.println("A data=" + Data3.count);
+
+        Data3 date2 = new Data3("B");
+        System.out.println("B data=" + Data3.count);
+
+        Data3 data3 = new Data3("C");
+        System.out.println("C data=" + Data3.count);
+    }
+
+}
