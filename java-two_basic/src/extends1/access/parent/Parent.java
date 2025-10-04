1fb97ab7cecddd39f5a2c3e7a15a2b8d16335a37
ansdudn2 <ansdudn2@naver.com>
2025-10-04 15:14:25 +0900

    feat: add printParent

--- java-two_basic/src/extends1/access/parent/Parent.java
+++ java-two_basic/src/extends1/access/parent/Parent.java
@@ -22,4 +22,16 @@ public class Parent {
     private void privateMethod() {
         System.out.println("parent.privateMethod");
     }
+
+    public void printParent() {
+        System.out.println("==Parent 메서드 안==");
+        System.out.println("publicValue = " + publicValue);
+        System.out.println("protectedValue = " + protectedValue);
+        System.out.println("defaultValue = " + defaultValue);
+        System.out.println("privateValue = " + privateValue);
+
+        // 부모 메서드 안에서 모두 접근 가능
+        defaultMethod();
+        privateMethod();
+    }
 }
