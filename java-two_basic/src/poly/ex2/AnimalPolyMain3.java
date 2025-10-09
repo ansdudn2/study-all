99db843f2964594d0250c76ed7d51dc1bbfa3f7f
ansdudn2 <ansdudn2@naver.com>
2025-10-10 00:45:03 +0900

    feat: add Pig class extending Animal

--- java-two_basic/src/poly/ex2/AnimalPolyMain3.java
+++ java-two_basic/src/poly/ex2/AnimalPolyMain3.java
@@ -2,7 +2,7 @@ package poly.ex2;
 
 public class AnimalPolyMain3 {
     public static void main(String[] args) {
-        Animal[] animalArr = {new Dog(), new Cat(), new Caw()};
+        Animal[] animalArr = {new Dog(), new Cat(), new Caw(),new Pig()};
 
         for (Animal animal : animalArr) {
             soundAnimal(animal);
