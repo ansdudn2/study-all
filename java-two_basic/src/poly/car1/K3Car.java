53869d1139ce3cc7038f63214581ae1bc850477b
ansdudn2 <ansdudn2@naver.com>
2025-10-11 04:06:21 +0900

    feat: extend CarMain1 to demonstrate OCP with new car implementation

--- java-two_basic/src/poly/car1/K3Car.java
+++ java-two_basic/src/poly/car1/K3Car.java
@@ -4,16 +4,16 @@ public class K3Car implements Car {
 
     @Override
     public void startEngine() {
-        System.out.println("k3Car startEngine");
+        System.out.println("k3Car.startEngine");
     }
 
     @Override
     public void offEngine() {
-        System.out.println("k3Car offEngine");
+        System.out.println("k3Car.offEngine");
     }
 
     @Override
     public void pressAccelerator() {
-        System.out.println("k3Car pressAccelerator");
+        System.out.println("k3Car.pressAccelerator");
     }
 }
