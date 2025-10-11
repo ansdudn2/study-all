2633ff9d421a666de2ae20fb879698270006d4ee
ansdudn2 <ansdudn2@naver.com>
2025-10-12 02:57:26 +0900

    feat: verify that toString() from Object is inherited in Child

--- java-mid1/src/lang/object/ObjectMain.java
+++ java-mid1/src/lang/object/ObjectMain.java
@@ -6,5 +6,9 @@ public class ObjectMain {
         Child child = new Child();
         child.childMethod();
         child.parentMethod();
+
+        // toString()은 Object 클래스의 메서드
+        String string = child.toString();
+        System.out.println(string);
     }
 }
