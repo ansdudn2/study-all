33c60e3e65323026704b8a4e5dd8c8125a507f5a
ansdudn2 <ansdudn2@naver.com>
2025-10-12 02:46:52 +0900

    feat: test System class without explicit java.lang import

--- java-mid1/src/lang/LangMain.java
+++ java-mid1/src/lang/LangMain.java
@@ -1,4 +1,11 @@
 package lang;
 
+// 생략된 임포트
+import java.lang.System;
+
 public class LangMain {
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
 }
