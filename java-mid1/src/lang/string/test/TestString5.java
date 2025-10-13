c33d025f116ed792f31d5afd076e978d91e95d3d
ansdudn2 <ansdudn2@naver.com>
2025-10-14 04:22:02 +0900

    chore: setup base template for TestString5 problem

---
+++ java-mid1/src/lang/string/test/TestString5.java
@@ -0,0 +1,9 @@
+package lang.string.test;
+
+public class TestString5 {
+    public static void main(String[] args) {
+        String str = "hello.txt";
+        String ext = ".txt";
+        // 코드 작성
+    }
+}
