3e1cac9b42db6925e38d1bcb91a65c6f115b5136
ansdudn2 <ansdudn2@naver.com>
2025-10-14 04:39:20 +0900

    feat: solve TestString7 by trimming whitespace from string

--- java-mid1/src/lang/string/test/TestString7.java
+++ java-mid1/src/lang/string/test/TestString7.java
@@ -4,5 +4,7 @@ public class TestString7 {
     public static void main(String[] args) {
         String original = "        Hello Java       ";
         // 코드 작성
+        String result = original.trim();
+        System.out.println(result);
     }
 }
