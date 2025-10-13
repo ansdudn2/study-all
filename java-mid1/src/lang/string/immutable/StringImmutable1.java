5944e191721164c46a178d738caad65e905235fd
ansdudn2 <ansdudn2@naver.com>
2025-10-14 02:33:14 +0900

    feat: add StringImmutable2 example showing new String on concat

--- java-mid1/src/lang/string/immutable/StringImmutable1.java
+++ java-mid1/src/lang/string/immutable/StringImmutable1.java
@@ -1,6 +1,6 @@
 package lang.string.immutable;
 
-public class StringImmutable {
+public class StringImmutable1 {
     public static void main(String[] args) {
         String str = "hello";
         str.concat(" java");
