ed0b52f2eee418e7a631ceaf3d25731a9e6bbc0b
ansdudn2 <ansdudn2@naver.com>
2025-10-14 04:45:06 +0900

    fix: replace replaceAll() with replace() for simple string substitution

--- java-mid1/src/lang/string/test/TestString8.java
+++ java-mid1/src/lang/string/test/TestString8.java
@@ -4,7 +4,7 @@ public class TestString8 {
     public static void main(String[] args) {
         String input = "hello java spring jpa java";
         // 코드 작성
-        String result = input.replaceAll("java", "jvm");
+        String result = input.replace("java", "jvm");
         System.out.println(result);
     }
 }
