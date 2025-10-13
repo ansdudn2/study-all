61108c470be62a8eed35d7415c99bd1916c47913
ansdudn2 <ansdudn2@naver.com>
2025-10-14 04:07:00 +0900

    chore: add initial code template for TestString2 with String array initialization

---
+++ java-mid1/src/lang/string/test/TestString2.java
@@ -0,0 +1,8 @@
+package lang.string.test;
+
+public class TestString2 {
+    public static void main(String[] args) {
+        String[] arr = {"hello", "java", "jvm", "spring", "jpa"};
+        // 코드 작성
+    }
+}
