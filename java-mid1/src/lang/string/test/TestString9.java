187d22762b40fccc4e2613a30b52136bf5666b33
ansdudn2 <ansdudn2@naver.com>
2025-10-14 04:48:31 +0900

    feat: solve TestString9 by splitting email into id and domain

--- java-mid1/src/lang/string/test/TestString9.java
+++ java-mid1/src/lang/string/test/TestString9.java
@@ -1,6 +1,13 @@
 package lang.string.test;
 
 public class TestString9 {
-    String email = "hello@example.com";
-    // 코드 작성
+    public static void main(String[] args) {
+        String email = "hello@example.com";
+        // 코드 작성
+        String[] parts = email.split("@");
+        String idparts = parts[0];
+        String domainparts = parts[1];
+        System.out.println("ID: " + idparts);
+        System.out.println("Domain: " + domainparts);
+    }
 }
