5ecf85845b711f8e39e8355254bb5bfadb7ebb2f
ansdudn2 <ansdudn2@naver.com>
2025-10-15 03:26:49 +0900

    feat: implement string to double conversion and summation in WrapperTest2

--- java-mid1/src/lang/wrapper/test/WrapperTest2.java
+++ java-mid1/src/lang/wrapper/test/WrapperTest2.java
@@ -5,6 +5,11 @@ public class WrapperTest2 {
         String[] array = {"1.5", "2.5", "3.0" };
 
         // 코드 작성
-
+        double sum = 0;
+        for (String arr : array){
+            double i = Double.parseDouble(arr);
+            sum += i;
+        }
+        System.out.println("sum = " + sum);
     }
 }
