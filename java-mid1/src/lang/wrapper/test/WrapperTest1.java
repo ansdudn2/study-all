dce50827ea98644c9073486e77709de1de72618f
ansdudn2 <ansdudn2@naver.com>
2025-10-15 03:22:29 +0900

    feat: implement string to integer conversion and summation in WrapperTest1

--- java-mid1/src/lang/wrapper/test/WrapperTest1.java
+++ java-mid1/src/lang/wrapper/test/WrapperTest1.java
@@ -5,5 +5,9 @@ public class WrapperTest1 {
         String str1 = "10";
         String str2 = "20";
         // 코드 작성
+        int int1 = Integer.parseInt(str1);
+        int int2 = Integer.parseInt(str2);
+        int sum = int1 + int2;
+        System.out.println("두 수의 합: " + sum);
     }
 }
