5b6099d81f7c89dc2184e38c2dabc6c9f0cb1743
ansdudn2 <ansdudn2@naver.com>
2025-10-15 02:16:21 +0900

    ref: add auto-boxing and auto-unboxing examples to AutoboxingMain1

--- java-mid1/src/lang/wrapper/AutoboxingMain1.java
+++ java-mid1/src/lang/wrapper/AutoboxingMain1.java
@@ -5,11 +5,15 @@ public class AutoboxingMain1 {
         // Primitive -> Wrapper
         int value = 7;
         Integer boxedValue = Integer.valueOf(value);
+        Integer boxedValue2 = value; // 오토 박싱
 
         // Wrapper -> Primitive
         int unboxedValue = boxedValue.intValue();
+        int unboxedValue2 = boxedValue2; // 오토 언박싱
 
         System.out.println("boxedValue = " + boxedValue);
         System.out.println("unboxedValue = " + unboxedValue);
+        System.out.println("boxedValue2 = " + boxedValue2);
+        System.out.println("unboxedValue2 = " + unboxedValue2);
     }
 }
