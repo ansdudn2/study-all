14ad2c26b534302f499cd0c44d8aeb7e3e2eadd8
ansdudn2 <ansdudn2@naver.com>
2025-10-16 00:50:59 +0900

    feat: implement DiscountService using string-based grade comparison

--- java-mid1/src/enumeration/ex0/DiscountService.java
+++ java-mid1/src/enumeration/ex0/DiscountService.java
@@ -1,4 +1,19 @@
 package enumeration.ex0;
 
 public class DiscountService {
+    int discountPercent = 0;
+
+    public int discount(String grade, int price) {
+        if (grade.equals("BASIC")) {
+            discountPercent = 10;
+        }  else if (grade.equals("GOLD")) {
+            discountPercent = 20;
+        } else if (grade.equals("DIAMOND")) {
+            discountPercent = 30;
+        } else {
+            System.out.println(grade + ": 할인X");
+        }
+
+        return price * discountPercent / 100;
+    }
 }
