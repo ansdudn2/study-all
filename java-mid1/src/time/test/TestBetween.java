a59fc57f8101735a11a211bc61be363d2e8eacd6
ansdudn2 <ansdudn2@naver.com>
2025-10-17 04:46:51 +0900

    chore: add TestBetween problem description for D-day calculation

---
+++ java-mid1/src/time/test/TestBetween.java
@@ -0,0 +1,11 @@
+package time.test;
+
+import java.time.LocalDate;
+
+public class TestBetween {
+    public static void main(String[] args) {
+        LocalDate startDate = LocalDate.of(2024, 1, 1);
+        LocalDate endDate = LocalDate.of(2024, 11, 21);
+        // 코드 작성
+    }
+}
