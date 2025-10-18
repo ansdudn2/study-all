fd614429277c8d996317b6134c57f10b80cf2b32
ansdudn2 <ansdudn2@naver.com>
2025-10-18 23:15:22 +0900

    chore: add problem template for static nested class implementation

---
+++ java-mid1/src/nested/test/OuterClass1.java
@@ -0,0 +1,5 @@
+package nested.test;
+
+public class OuterClass1 {
+    // 여기에 NestedClass를 구현해라. 그리고 hello() 메서드를 만들어라.
+}
---
+++ java-mid1/src/nested/test/OuterClass1Main.java
@@ -0,0 +1,7 @@
+package nested.test;
+
+public class OuterClass1Main {
+    public static void main(String[] args) {
+        // 여기에서 NestedClass의 hello() 메서드를 호출하라.
+    }
+}
