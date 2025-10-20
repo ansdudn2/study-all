b1fc250b263a4c867c068384b2a1712f8fa5bba9
ansdudn2 <ansdudn2@naver.com>
2025-10-20 19:29:43 +0900

    feat: add NetworkServiceV2_1 and reformat NetworkClientV2 for clean structure

--- java-mid1/src/exception/ex2/NetworkClientV2.java
+++ java-mid1/src/exception/ex2/NetworkClientV2.java
@@ -3,14 +3,14 @@ package exception.ex2;
 public class NetworkClientV2 {
     private final String address;
     public boolean connectError;
-    public  boolean sendError;
+    public boolean sendError;
 
-    public NetworkClientV2(String address){
+    public NetworkClientV2(String address) {
         this.address = address;
     }
 
-    public void connect() throws NetworkClientExceptionV2{
-        if (connectError){
+    public void connect() throws NetworkClientExceptionV2 {
+        if (connectError) {
             throw new NetworkClientExceptionV2("connectError", address + "서버 연결 실패");
         }
 
@@ -18,23 +18,24 @@ public class NetworkClientV2 {
         System.out.println(address + "서버 연결 성공");
     }
 
-    public void send(String data) throws NetworkClientExceptionV2{
-        if (sendError){
-            throw new NetworkClientExceptionV2("sendError",address+"서버에 데이터 정송 실패: " + data );
+    public void send(String data) throws NetworkClientExceptionV2 {
+        if (sendError) {
+            throw new NetworkClientExceptionV2("sendError", address + "서버에 데이터 정송 실패: " + data);
         }
 
         // 전송 성공
         System.out.println(address + "서버에 데이터 전송: " + data);
     }
+
     public void disconnect() {
         System.out.println(address + "서버 연결 해제");
     }
 
-    public void initError(String data){
-        if (data.contains("error1")){
+    public void initError(String data) {
+        if (data.contains("error1")) {
             connectError = true;
         }
-        if (data.contains("error2")){
+        if (data.contains("error2")) {
             sendError = true;
         }
     }
