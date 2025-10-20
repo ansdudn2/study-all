f724139e90d66ed0075ccbdff73f3d14664f405c
ansdudn2 <ansdudn2@naver.com>
2025-10-20 23:41:40 +0900

    ref: update MainV4 to use NetworkServiceV5 with try-with-resources support

--- java-mid1/src/exception/ex4/MainV4.java
+++ java-mid1/src/exception/ex4/MainV4.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class MainV4 {
     public static void main(String[] args) {
 
-        NetworkServiceV4 networkService = new NetworkServiceV4();
+        //NetworkServiceV4 networkService = new NetworkServiceV4();
+        NetworkServiceV5 networkService = new NetworkServiceV5();
         Scanner sc = new Scanner(System.in);
         while (true) {
             System.out.print("전송할 문자: ");
