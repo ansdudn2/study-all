fa983f54840421c98dbc139c6f19edf559a4c95f
ansdudn2 <ansdudn2@naver.com>
2025-10-20 21:37:39 +0900

    feat: define NetworkClientExceptionV3 as checked base exception for exception.ex3 hierarchy

--- java-mid1/src/exception/ex3/NetworkClientExceptionV3.java
+++ java-mid1/src/exception/ex3/NetworkClientExceptionV3.java
@@ -1,6 +1,6 @@
 package exception.ex3;
 
-public class NetworkClientExceptionV3 extends RuntimeException {
+public class NetworkClientExceptionV3 extends Exception {
     public NetworkClientExceptionV3(String message) {
         super(message);
     }
