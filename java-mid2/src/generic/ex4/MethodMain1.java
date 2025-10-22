7d06d3486d1f1ee1b03a6f8e87e3c369796adab5
ansdudn2 <ansdudn2@naver.com>
2025-10-22 20:39:27 +0900

    feat: add type inference examples following explicit type argument demo

--- java-mid2/src/generic/ex4/MethodMain1.java
+++ java-mid2/src/generic/ex4/MethodMain1.java
@@ -10,5 +10,10 @@ public class MethodMain1 {
         Integer result = GenericMethod.<Integer>genericMethod(i);
         Integer integerValue = GenericMethod.<Integer>numberMethod(10);
         Double doubleValue = GenericMethod.<Double>numberMethod(20.0);
+
+        System.out.println("타입 추론");
+        Integer result1 = GenericMethod.genericMethod(i);
+        Integer integerValue1 = GenericMethod.numberMethod(10);
+        Double doubleValue1 = GenericMethod.numberMethod(20.0);
     }
 }
