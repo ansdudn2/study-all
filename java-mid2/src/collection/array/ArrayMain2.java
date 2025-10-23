e1e687d4cd0610a35734e6bf95b748a46bf40670
ansdudn2 <ansdudn2@naver.com>
2025-10-23 15:46:30 +0900

    ref: reorder array insertion methods in ArrayMain2

--- java-mid2/src/collection/array/ArrayMain2.java
+++ java-mid2/src/collection/array/ArrayMain2.java
@@ -28,9 +28,11 @@ public class ArrayMain2 {
         addLast(arr, 5);
         System.out.println(Arrays.toString(arr));
     }
-
-    private static void addLast(int[] arr, int newValue) {
-        arr[arr.length - 1] = newValue;
+    private static void addFirst(int[] arr, int newValue) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = newValue;
     }
 
     private static void addAtIndex(int[] arr, int index, int newValue) {
@@ -40,10 +42,8 @@ public class ArrayMain2 {
         arr[index] = newValue;
     }
 
-    private static void addFirst(int[] arr, int newValue) {
-        for (int i = arr.length - 1; i > 0; i--) {
-            arr[i] = arr[i - 1];
-        }
-        arr[0] = newValue;
+
+    private static void addLast(int[] arr, int newValue) {
+        arr[arr.length - 1] = newValue;
     }
 }
