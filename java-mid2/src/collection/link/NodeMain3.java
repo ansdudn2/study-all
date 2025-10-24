d3417297487c7ae20d022000241c894a63f0ab16
ansdudn2 <ansdudn2@naver.com>
2025-10-25 02:27:00 +0900

    chore: format NodeMain3 by fixing indentation and comment spacing

--- java-mid2/src/collection/link/NodeMain3.java
+++ java-mid2/src/collection/link/NodeMain3.java
@@ -3,27 +3,27 @@ package collection.link;
 public class NodeMain3 {
 
     public static void main(String[] args) {
-        //노드 생성하고 연결하기: A -> B -> C
+        // 노드 생성하고 연결하기: A -> B -> C
         Node first = new Node("A");
         first.next = new Node("B");
         first.next.next = new Node("C");
 
         System.out.println(first);
 
-        //모든 노드 탐색하기
+        // 모든 노드 탐색하기
         System.out.println("모든 노드 탐색하기");
         printAll(first);
 
-        //마지막 노드 조회하기
+        // 마지막 노드 조회하기
         Node lastNode = getLastNode(first);
         System.out.println("lastNode = " + lastNode);
 
-        //특정 index의 노드 조회하기
+        // 특정 index의 노드 조회하기
         int index = 2;
         Node index2Node = getNode(first, index);
         System.out.println("index2Node = " + index2Node.item);
 
-        //데이터 추가하기
+        // 데이터 추가하기
         System.out.println("데이터 추가하기");
         add(first, "D");
         System.out.println(first);
