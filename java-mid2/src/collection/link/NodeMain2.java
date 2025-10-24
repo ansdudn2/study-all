807364a9e5e6e8548fa22f5a44eeb9cd68ec68b0
ansdudn2 <ansdudn2@naver.com>
2025-10-25 02:26:24 +0900

    chore: fix spacing and indentation in NodeMain2

--- java-mid2/src/collection/link/NodeMain2.java
+++ java-mid2/src/collection/link/NodeMain2.java
@@ -3,7 +3,7 @@ package collection.link;
 public class NodeMain2 {
 
     public static void main(String[] args) {
-        //노드 생성하고 연결하기: A -> B -> C
+        // 노드 생성하고 연결하기: A -> B -> C
         Node first = new Node("A");
         first.next = new Node("B");
         first.next.next = new Node("C");
