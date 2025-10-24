3073b8eba66828dd4bfaaa166456f6a3d4a5d42d
ansdudn2 <ansdudn2@naver.com>
2025-10-24 23:57:52 +0900

    ref: improve Node toString for concise linked list representation

--- java-mid2/src/collection/link/Node.java
+++ java-mid2/src/collection/link/Node.java
@@ -9,13 +9,29 @@ public class Node {
         this.item = item;
     }
 
-    //IDE 생성 toString()
+    /*
+        //IDE 생성 toString()
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "item=" + item +
+                    ", next=" + next +
+                    '}';
+        }
+     */
     @Override
     public String toString() {
-        return "Node{" +
-                "item=" + item +
-                ", next=" + next +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
     }
-
 }
