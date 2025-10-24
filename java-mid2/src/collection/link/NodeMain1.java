d3db736e7e09529b5006f6523e8de21bd7cb7c29
ansdudn2 <ansdudn2@naver.com>
2025-10-25 02:15:47 +0900

    chore: adjust spacing and indentation in NodeMain1

--- java-mid2/src/collection/link/NodeMain1.java
+++ java-mid2/src/collection/link/NodeMain1.java
@@ -1,9 +1,8 @@
 package collection.link;
 
 public class NodeMain1 {
-
     public static void main(String[] args) {
-        //노드 생성하고 연결하기: A -> B -> C
+        // 노드 생성하고 연결하기: A -> B -> C
         Node first = new Node("A");
         first.next = new Node("B");
         first.next.next = new Node("C");
