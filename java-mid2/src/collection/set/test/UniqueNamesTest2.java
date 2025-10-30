102e63b6a92f3a1d3dc5b1223626aeedd4ecf5e7
ansdudn2 <ansdudn2@naver.com>
2025-10-30 14:24:46 +0900

    feat: implement UniqueNamesTest2 solution preserving insertion order using LinkedHashSet

---
+++ java-mid2/src/collection/set/test/UniqueNamesTest2.java
@@ -0,0 +1,10 @@
+package collection.set.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueNamesTest2 {
+    public static void main(String[] args) {
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+    }
+}
