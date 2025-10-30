6ccced1298c386972576caa9fe217e52f1bb0f93
ansdudn2 <ansdudn2@naver.com>
2025-10-30 14:31:06 +0900

    ref: simplify UniqueNamesTest3 initialization using List.of for TreeSet

--- java-mid2/src/collection/set/test/UniqueNamesTest3.java
+++ java-mid2/src/collection/set/test/UniqueNamesTest3.java
@@ -6,8 +6,7 @@ import java.util.TreeSet;
 
 public class UniqueNamesTest3 {
     public static void main(String[] args) {
-        Integer[] inputArr = {30, 20, 20, 10, 10};
-        Set<Integer> set = new TreeSet<>(List.of(inputArr));
+        Set<Integer> set = new TreeSet<>(List.of(30, 20, 20, 10, 10));
 
         for (Integer s : set) {
             System.out.println(s);
