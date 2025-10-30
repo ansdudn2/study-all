9d2e3d1a99d4d197c86f84c5d8168e241171fddc
ansdudn2 <ansdudn2@naver.com>
2025-10-30 14:34:15 +0900

    feat: implement SetOperationsTest solution using addAll, retainAll, and removeAll

--- java-mid2/src/collection/set/test/SetOperationsTest.java
+++ java-mid2/src/collection/set/test/SetOperationsTest.java
@@ -9,5 +9,17 @@ public class SetOperationsTest {
         Set<Integer> set1 = new HashSet<>(List.of(1, 2, 3, 4, 5));
         Set<Integer> set2 = new HashSet<>(List.of(3, 4, 5, 6, 7));
 
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+
+        Set<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+
+        System.out.println("합집합: " + union);
+        System.out.println("교집합: " + intersection);
+        System.out.println("차집합: " + difference);
     }
 }
