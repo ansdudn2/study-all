ca59063a96baa00b46cb520787a68497e51a7b3c
ansdudn2 <ansdudn2@naver.com>
2025-10-31 16:18:49 +0900

    feat: add WordFrequencyTest1 problem for counting occurrences of each word

---
+++ java-mid2/src/collection/map/test/WordFrequencyTest1.java
@@ -0,0 +1,9 @@
+package collection.map.test;
+
+public class WordFrequencyTest1 {
+    public static void main(String[] args) {
+        String text = "orange banana apple apple banana apple";
+
+        // 코드 작성
+    }
+}
