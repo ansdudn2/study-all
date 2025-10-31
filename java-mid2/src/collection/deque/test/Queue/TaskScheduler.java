64974e1ce57258566923179f55d409323ee2ec37
ansdudn2 <ansdudn2@naver.com>
2025-10-31 17:11:58 +0900

    feat: implement TaskScheduler with addTask, processNextTask, and getRemainingTasks using queue

--- java-mid2/src/collection/deque/test/Queue/TaskScheduler.java
+++ java-mid2/src/collection/deque/test/Queue/TaskScheduler.java
@@ -5,4 +5,20 @@ import java.util.Queue;
 
 public class TaskScheduler {
     private Queue<Task> tasks = new ArrayDeque<>();
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void processNextTask() {
+        Task task = tasks.poll();
+        if (task != null) {
+            task.execute();
+        }
+    }
+
+    public int getRemainingTasks() {
+        return tasks.size();
+    }
+
 }
