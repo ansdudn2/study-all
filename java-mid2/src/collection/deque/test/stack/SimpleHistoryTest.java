51421a1993d35c3c35a88af51e67fe757dedf347
ansdudn2 <ansdudn2@naver.com>
2025-10-31 16:56:24 +0900

    feat: add SimpleHistoryTest problem for practicing basic stack push and pop operations

---
+++ java-mid2/src/collection/deque/test/stack/SimpleHistoryTest.java
@@ -0,0 +1,11 @@
+package collection.deque.test.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SimpleHistoryTest {
+    public static void main(String[] args) {
+        Deque<String> stack = new ArrayDeque<>();
+
+    }
+}
