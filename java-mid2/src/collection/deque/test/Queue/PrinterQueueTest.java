607fee71349f9f7e54e7910c76842f5f9d821355
ansdudn2 <ansdudn2@naver.com>
2025-10-31 17:05:17 +0900

    feat: add PrinterQueueTest problem for simulating document print order using queue

---
+++ java-mid2/src/collection/deque/test/Queue/PrinterQueueTest.java
@@ -0,0 +1,10 @@
+package collection.deque.test.Queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class PrinterQueueTest {
+    public static void main(String[] args) {
+        Queue<String> printQueue = new ArrayDeque<>();
+    }
+}
