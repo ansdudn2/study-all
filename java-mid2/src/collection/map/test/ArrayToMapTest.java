2a351b3165b2dfc09a83da4ee6eb3cbc65b2ad49
ansdudn2 <ansdudn2@naver.com>
2025-10-31 16:04:53 +0900

    feat: add ArrayToMapTest problem for converting 2D array to Map

---
+++ java-mid2/src/collection/map/test/ArrayToMapTest.java
@@ -0,0 +1,8 @@
+package collection.map.test;
+
+public class ArrayToMapTest {
+    public static void main(String[] args) {
+        String[][] productArr = {{"Java", "10000"}, {"Spring", "20000"}, {"JPA", "30000"}};
+
+    }
+}
