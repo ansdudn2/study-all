cd68a46719c9fdaa1bcd71426e232a31a8f3c752
ansdudn2 <ansdudn2@naver.com>
2025-11-01 14:42:28 +0900

    feat: enhance SortMain3 to include IdComparator and reversed sorting examples

--- java-mid2/src/collection/compare/SortMain3.java
+++ java-mid2/src/collection/compare/SortMain3.java
@@ -15,5 +15,14 @@ public class SortMain3 {
         System.out.println("Comparable 기본 정렬");
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
+
+        // 추가
+        System.out.println("IdComparator 정렬");
+        Arrays.sort(array, new IdComparator());
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("IdComparator().reversed() 정렬");
+        Arrays.sort(array, new IdComparator().reversed());
+        System.out.println(Arrays.toString(array));
     }
 }
