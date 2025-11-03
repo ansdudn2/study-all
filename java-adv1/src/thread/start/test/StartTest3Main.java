5e2be8d19af783d1ec214ec56ac0874cf0ef9fc4
ansdudn2 <ansdudn2@naver.com>
2025-11-03 16:04:25 +0900

    feat: implement StartTest3Main with anonymous Runnable for exercise 3

--- java-adv1/src/thread/start/test/StartTest3Main.java
+++ java-adv1/src/thread/start/test/StartTest3Main.java
@@ -4,23 +4,22 @@ import static util.MyLogger.log;
 
 public class StartTest3Main {
     public static void main(String[] args) {
-        Thread thread = new Thread(new CounterRunnable(), "counter");
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 5; i++) {
+                    log("value: " + i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        };
+        Thread thread = new Thread(runnable, "counter");
         thread.start();
 
     }
-
-    static class CounterRunnable implements Runnable {
-        @Override
-        public void run() {
-            for (int i = 1; i <= 5; i++) {
-                log("value: " + i);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-    }
-
 }
