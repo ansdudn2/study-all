8155e62cd71cb571040da8d60340f7af1303e4b0
ansdudn2 <ansdudn2@naver.com>
2025-11-03 15:30:33 +0900

    feat: refactor MyLogger to public static and add MyLoggerMain for test

--- java-adv1/src/util/MyLogger.java
+++ java-adv1/src/util/MyLogger.java
@@ -4,10 +4,10 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class MyLogger {
-    private static final DateTimeFormatter fomatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
 
-    private static void log(Object obj) {
-        String time = LocalTime.now().format(fomatter);
+    public static void log(Object obj) {
+        String time = LocalTime.now().format(formatter);
         System.out.printf("%s [%9s] %s\n", time, Thread.currentThread().getName(), obj);
     }
 }
